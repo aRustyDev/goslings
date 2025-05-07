d1b5b74362f10247ca081905afd0de4c1ff5db40
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-05-07 11:44:11 -0400

    refactor: moved viper config logic internal/app -> internal/conf

--- internal/app/cli/cmd/root.go
+++ internal/app/cli/cmd/root.go
@@ -2,72 +2,32 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"strings"
 
-	// homedir "github.com/mitchellh/go-homedir"
+	"goslings/internal/conf"
+	"goslings/internal/utils"
+
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // define flags and handle configuration
 func init() {
 
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(conf.InitConfig)
 
 	rootCmd.AddCommand(honkCmd)
 	rootCmd.AddCommand(confCmd)
 	rootCmd.AddCommand(dumpCmd)
 	rootCmd.AddCommand(authCmd)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringVarP(&projectBase, "projectbase", "b", "", "base project directory eg. github.com/spf13/")
-	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "Author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "Name of license for the project (can provide `licensetext` in config)")
-	rootCmd.PersistentFlags().Bool("viper", true, "Use Viper for configuration")
-
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("projectbase", rootCmd.PersistentFlags().Lookup("projectbase"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-
-	viper.SetDefault("author", "Adam Smith <developer@gh.arusty.dev>")
-	viper.SetDefault("license", "agpl3")
-}
-
-func initConfig() {
-	viper.SetConfigName("brood") // Config file name without extension
-	viper.SetConfigType("yaml")  // Config file type
-	viper.AddConfigPath(".")     // Look for the config file in the current directory
-
-	/*
-	   AutomaticEnv will check for an environment variable any time a viper.Get request is made.
-	   It will apply the following rules.
-	       It will check for an environment variable with a name matching the key uppercased and prefixed with the EnvPrefix if set.
-	*/
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("env")                              // will be uppercased automatically
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // this is useful e.g. want to use . in Get() calls, but environmental variables to use _ delimiters (e.g. app.port -> APP_PORT)
-
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
-
-	// Set up environment variable mappings if necessary
-	/*
-	   BindEnv takes one or more parameters. The first parameter is the key name, the rest are the name of the environment variables to bind to this key.
-	   If more than one are provided, they will take precedence in the specified order. The name of the environment variable is case sensitive.
-	   If the ENV variable name is not provided, then Viper will automatically assume that the ENV variable matches the following format: prefix + "_" + the key name in ALL CAPS.
-	   When you explicitly provide the ENV variable name (the second parameter), it does not automatically add the prefix.
-	       For example if the second parameter is "id", Viper will look for the ENV variable "ID".
-	*/
-	viper.BindEnv("app.name", "APP_NAME") // Bind the app.name key to the APP_NAME environment variable
-
+	rootCmd.PersistentFlags().StringP("author", "a", "Adam Smith", "Author name for copyright attribution")
+	// rootCmd.PersistentFlags().Bool("viper", true, "Use Viper for configuration")
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "gosling",
-	Short: "Goslings is a cloud-native MSFT Cloud IR tool",
+	Use:     "gosling",
+	Short:   "Goslings is a cloud-native MSFT Cloud IR tool",
+	Version: utils.Version,
 	Long: `The Goslings Tool is a robust and flexible hunt and incident
 			response tool that adds novel authentication and data gathering
 			methods in order to run a full investigation against a customer’s
---
+++ internal/conf/viper.go
@@ -0,0 +1,47 @@
+package conf
+
+import (
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func InitConfig() {
+	viper.SetConfigName("brood")              // Config file name without extension
+	viper.SetConfigType("yaml")               // Config file type
+	viper.AddConfigPath(".")                  // 1st: Look for the config file in the current directory
+	viper.AddConfigPath("./configs")          // 2nd: Look for the config file in the local configs directory
+	viper.AddConfigPath("~/.config/goslings") // 3rd: Look for the config file in the home config directory
+
+	/*
+	   AutomaticEnv will check for an environment variable any time a viper.Get request is made.
+	   It will apply the following rules.
+	       It will check for an environment variable with a name matching the key uppercased and prefixed with the EnvPrefix if set.
+	*/
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("env")                              // will be uppercased automatically
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // this is useful e.g. want to use . in Get() calls, but environmental variables to use _ delimiters (e.g. app.port -> APP_PORT)
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	// Set up environment variable mappings if necessary
+	/*
+	   BindEnv takes one or more parameters. The first parameter is the key name, the rest are the name of the environment variables to bind to this key.
+	   If more than one are provided, they will take precedence in the specified order. The name of the environment variable is case sensitive.
+	   If the ENV variable name is not provided, then Viper will automatically assume that the ENV variable matches the following format: prefix + "_" + the key name in ALL CAPS.
+	   When you explicitly provide the ENV variable name (the second parameter), it does not automatically add the prefix.
+	       For example if the second parameter is "id", Viper will look for the ENV variable "ID".
+	*/
+	viper.BindEnv("app.name", "APP_NAME") // Bind the app.name key to the APP_NAME environment variable
+
+	// viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	// viper.BindPFlag("projectbase", rootCmd.PersistentFlags().Lookup("projectbase"))
+	// viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+
+	viper.SetDefault("author", "Adam Smith <developer@gh.arusty.dev>")
+	viper.SetDefault("license", "agpl3")
+
+}
